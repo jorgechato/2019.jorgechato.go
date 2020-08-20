e17c8ab76f603f38563bd2be5156af97ba9df41c
jorgechato <hack@jorgechato.com>
2020-08-20 10:26:43 +0200

    UPDATE router build

--- pkg/location/infrastructure/api/router.go
+++ pkg/location/infrastructure/api/router.go
@@ -1,18 +1,16 @@
 package api
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	"jorgechato.com/pkg/location/application"
 )
 
-func Router(APIBASE string, router *gin.Engine) {
+func Router(BASE string, router *gin.Engine) {
 	var service application.LocationUseCase
 	handler := NewHandler(service)
 
-	v := router.Group(fmt.Sprintf("%s/location", APIBASE))
+	v := router.Group(BASE + "/location")
 	{
 		v.GET("/current", handler.GetCurrent)
 		v.GET("/next", handler.GetNext)
--- pkg/location/infrastructure/polarsteps/constants.go
+++ pkg/location/infrastructure/polarsteps/constants.go
@@ -1,9 +1,11 @@
 package polarsteps
 
 const (
-	PROFILE_API    = "https://api.polarsteps.com/users/byusername/%v"
-	TRIPS_API      = "https://api.polarsteps.com/trips/%v"
-	POLARSTEP_USER = "POLARSTEP_USER"
+	BASE_API = "api.polarsteps.com"
+	PROFILE  = "users/byusername/"
+	TRIPS    = "trips/%v"
+
+	USER = "POLARSTEP_USER"
 
 	TAG = "polarsteps_planned"
 )
