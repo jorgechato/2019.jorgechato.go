f19a359144bb9d32589001ef96f7767f4171881e
jorgechato <hack@jorgechato.com>
2020-08-21 08:21:16 +0200

    FIX typo

--- pkg/books/domain/model.go
+++ pkg/books/domain/model.go
@@ -8,14 +8,14 @@ type (
 	Book struct {
 		Score    string `json:"score" xml:"rating"`
 		Metadata struct {
-			Title     string  `json:"title" xml:"title_without_series"`
-			Cover     string  `json:"cover" xml:"image_url"`
-			Goodreads string  `json:"goodreads_url" xml:"link"`
-			Autors    []Autor `json:"author_list" xml:"authors>author"`
+			Title     string   `json:"title" xml:"title_without_series"`
+			Cover     string   `json:"cover" xml:"image_url"`
+			Goodreads string   `json:"goodreads_url" xml:"link"`
+			Authors   []Author `json:"author_list" xml:"authors>author"`
 		} `json:"book" xml:"book"`
 	}
 
-	Autor struct {
+	Author struct {
 		Name string `json:"name" xml:"name"`
 	}
 )
