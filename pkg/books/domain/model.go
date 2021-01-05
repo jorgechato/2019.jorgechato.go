57160a8376f090f16694ae723f8e38589312bd70
jorgechato <hack@jorgechato.com>
2021-01-05 12:31:16 +0100

    ADD isbn code in books module

--- pkg/books/domain/model.go
+++ pkg/books/domain/model.go
@@ -12,6 +12,7 @@ type (
 			Cover     string   `json:"cover" xml:"image_url"`
 			Goodreads string   `json:"goodreads_url" xml:"link"`
 			Authors   []Author `json:"author_list" xml:"authors>author"`
+			ISBN      string   `json:"isbn" xml:"isbn"`
 		} `json:"book" xml:"book"`
 	}
 
